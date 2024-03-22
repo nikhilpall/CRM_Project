afcc67bf62fc5a07c707de6fce4b3eb138622459
Nikhil <pallnikhil2000@gmail.com>
2024-03-22 15:06:32 +0530

    Uploading CRM Model Requirement

---
+++ src/test/java/com/nikhil/main/CrmProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhil.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
